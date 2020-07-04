fd72c38de489cc9ee893a9830fbc831c2cb8451f
Yuuka Motobayashi <yuukanon@gmail.com>
2020-07-04 12:25:22 +0200

    navbar

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+     
+        <div id = "headerimg" img src="https://images.unsplash.com/photo-1489533119213-66a5cd877091?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80" alt="" />
+      
+  )
+}
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css';
+
+
+const Navbar = () => {
+  return(
+     <navbar>
+       <ul>
+         <li>Profile Summary</li>
+         <li>Skills</li>
+         <li>Projects</li>
+         <li>Contact</li>
+       </ul>
+     </navbar>
+       
+      
+  )
+}
+
+export default Navbar;
