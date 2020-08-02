f5d8c3649582feeeae3f9a480119b15e84733e76
Yuuka Motobayashi <yuukanon@gmail.com>
2020-08-02 14:49:09 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
@@ -12,31 +12,9 @@ import Quotes from './components/Quotes';
 import Header2 from './components/Header2'
 
 
- class App extends Component {
-   constructor(props) {
-     super(props);
-     this.state = {
-       data: {},
-       isloaded: false,
-     }
-     }
+ function App() {
+   
 
-     componentDidMount() {
-       this.fetchData()
-     }
-
-     fetchData = () => {
-       fetch(`http://quotes.rest/qod.json?category=inspire`)
-       .then(res => res.json())
-       .then(data => {this.setState({
-         data,
-         isloaded: true,
-       })
-      })
-     }
-
-     render() {
-       const {data, isloaded} = this.state;
        return(
         <div className="App">
             <title>Yuuka Motobayashi Portfolio</title>
@@ -45,14 +23,13 @@ import Header2 from './components/Header2'
                 <Route exact path='/' component={Header} />
                 <Route path='/summary' component={Summary} />
                 <Route path='/skill' component={SkillList} />
-                <Route path='/quotes' render={() => isloaded && <Quotes data={data} />} />
                 <Route path='/project' component={Project} />
                 <Route path='/contact' component={Contact} />
               </Switch>
         </div>
        )
      }
-   }
+   
  
 
 export default App;
